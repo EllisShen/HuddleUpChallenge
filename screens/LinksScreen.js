561696eb87850d0e1b2bf41c429b2e13e9c77465
Ellis Shen <weichu.shen@ntdtv.com>
2017-01-30 00:47:57 -0500

    [Feature] Implement Task 2 to show my favorite foods
    * Scroll down to the bottom will load more data

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -5,34 +5,40 @@ import {
     Text,
     View,
     ListView,
+    Dimensions,
+    ActivityIndicator,
 } from 'react-native';
-import {
-    ExponentLinksView,
-} from '@exponent/samples';
 
 export default class LinksScreen extends React.Component {
-    static route = {
-        navigationBar: {
-            title: 'Links',
-        },
-    }
-
     constructor() {
         super();
+
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
         this.state = {
-            dataSource: ds.cloneWithRows(['McCafe Mocha, Large - calories: 500',
-                'McCafe Hot Chocolate, Small - calories: 360',
-                'Etc.']),
+            dataSource: ds.cloneWithRows([]),
+            isLoading: false,
         };
+
+        this._data = [];
+        this._dataLength = 0;
+        this._dataLowerIndex = 0;
+        this._dataUpperIndex = 50;
+        this._fetchFoodList = this._fetchFoodList.bind(this);
+        this._renderRow = this._renderRow.bind(this);
+    }
+
+    componentDidMount() {
+        // fetch foodList
+        this._fetchFoodList();
     }
 
     render() {
+        const {dataSource, isLoading} = this.state;
+
         return (
-            <ScrollView
-                style={styles.container}
-                contentContainerStyle={this.props.route.getContentContainerStyle()}>
-                <View>
+            <View style={styles.container}>
+                <View style={styles.taskDescription}>
                     <Text style={styles.welcomeText}>Nutrition Info ListView</Text>
                     <Text style={styles.descriptionText}>On this screen you'll use the ListView component and the
                         Nutritionix API (https://www.nutritionix.com/business/api).</Text>
@@ -41,14 +47,101 @@ export default class LinksScreen extends React.Component {
                         of your favorite fast food restaurant. Feel free to add styling, embellishments, or any other
                         features you'd like!</Text>
                 </View>
-                <Text style={styles.welcomeText}>McDonalds!</Text>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Text>{rowData}</Text>}
-                />
 
-            </ScrollView>
+                <View style={styles.sectionContainer}>
+                    <Text style={styles.sectionTitle}>Starbucks Menu ({this._data.length})</Text>
+                </View>
+                <ListView
+                    dataSource={dataSource}
+                    renderRow={this._renderRow.bind(this)}
+                    onEndReached={this._onEndReached.bind(this)}
+                    renderSeparator={(sectionID, rowID) => <View key={`${rowID}`} style={styles.separator}/>}
+                    enableEmptySections={true}
+                    initialListSize={50}
+                    pageSize={50}
+                    style={styles.listContainer}
+                />
+                {
+                    isLoading &&
+                    this._showIndicator()
+                }
+            </View>
         );
+    }
+
+    _showIndicator() {
+        return (
+            <View style={{backgroundColor: '#f1f1e8'}}>
+                <ActivityIndicator size={'large'} color={'lightgray'}/>
+            </View>
+        )
+    }
+
+    _onEndReached() {
+        this._dataLowerIndex += 50;
+        this._dataUpperIndex += 50;
+        if (this._dataLowerIndex > this._dataLength) this._dataLowerIndex = this._dataLength;
+
+        if (this._dataUpperIndex > this._dataLength) this._dataUpperIndex = this._dataLength;
+
+        this._fetchFoodList();
+    }
+
+    _renderRow(rowData, sectionID, rowID) {
+        const fields = rowData.fields;
+
+        return (
+            <View style={styles.rowContainer}>
+
+                <View style={styles.foodNameContainer}>
+                    <View style={{width: Dimensions.get('window').width * 0.7}}>
+                        <Text style={styles.foodTitle} numberOfLines={1}>
+                            {fields.item_name}
+                        </Text>
+                    </View>
+                    <Text style={styles.foodCalories}>
+                        {fields.nf_calories} calories
+                    </Text>
+                </View>
+                <View style={styles.detailButton}>
+                    <Text style={styles.detailbuttonText}>DETAILS</Text>
+                </View>
+            </View>
+        )
+    }
+
+    _fetchFoodList() {
+        // fetch images via Pixabay API
+        const data = {
+            baseUrl: 'https://api.nutritionix.com/v1_1/search/',
+            brand_id: '513fbc1283aa2dc80c00001f',
+            results: this._dataLowerIndex + ':' + this._dataUpperIndex,
+            cal_min: 0,
+            cal_max: 50000,
+            fields: '*',
+            appId: 'ef145797',
+            appKey: '8458752a99245bea8d24f01d025cceb5',
+        };
+
+        // show isLoading...
+        this.setState({isLoading: true});
+
+        fetch(`${data.baseUrl}?brand_id=${data.brand_id}&results=${data.results}&cal_min=${data.cal_min}
+        &cal_max=${data.cal_max}&fields=${data.fields}&appId=${data.appId}&appKey=${data.appKey}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((responseJson) => {
+                this._dataLength = responseJson.total_hits;
+                this._data = this._data.concat(responseJson.hits);
+                this.setState({
+                    isLoading: false,
+                    dataSource: this.state.dataSource.cloneWithRows(this._data),
+                })
+            })
+            .catch((error) => {
+                console.log(error)
+            });
     }
 
 }
@@ -56,13 +149,16 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 15,
+    },
+    taskDescription: {
+        marginBottom: 24,
+        maxHeight: 200,
     },
     descriptionText: {
         fontSize: 14,
         color: 'rgba(67,100,109, 1)',
         lineHeight: 15,
-        textAlign: 'left',
+        textAlign: 'center',
         paddingTop: 10,
     },
     welcomeText: {
@@ -71,5 +167,73 @@ const styles = StyleSheet.create({
         lineHeight: 23,
         textAlign: 'center',
         paddingTop: 55,
+    },
+    sectionContainer: {
+        paddingLeft: 16,
+        paddingTop: 4,
+        paddingBottom: 4,
+        backgroundColor: '#c0a666',
+    },
+    sectionTitle: {
+        fontFamily: 'sans-serif',
+        fontSize: 16,
+        textAlign: 'left',
+        color: 'white'
+    },
+    listContainer: {
+        maxHeight: Dimensions.get('window').height - 200 - 55.5,
+        backgroundColor: '#f1f1e8',
+    },
+    rowContainer: {
+        flex: 0,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginLeft: 8,
+        paddingTop: 16,
+        paddingBottom: 16,
+        marginRight: 8,
+        backgroundColor: '#fff9f8',
+    },
+    foodNameContainer: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        paddingLeft: 16,
+    },
+    foodTitle: {
+        fontFamily: 'sans-serif-medium',
+        fontSize: 16,
+        color: '#3A3A3A',
+        textAlign: 'left',
+    },
+    foodCalories: {
+        fontFamily: 'sans-serif',
+        fontSize: 12,
+        color: 'rgba(58, 58, 58, 1.00)',
+        textAlign: 'left',
+    },
+    detailButton: {
+        borderRadius: 4,
+        paddingTop: 2,
+        paddingBottom: 2,
+        paddingLeft: 4,
+        paddingRight: 4,
+        marginRight: 16,
+        backgroundColor: 'transparent',
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: '#c0a666',
+        alignItems: 'center'
+    },
+    detailbuttonText: {
+        textAlign: 'center',
+        fontSize: 12,
+        color: '#c0a666'
+    },
+    separator: {
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: '#EFEEED',
+        marginLeft: 16,
     },
 });
